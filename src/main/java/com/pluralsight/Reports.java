092d8052bf3c4929cef28feb4d0d3121f4706baa
Kenny Sosa <kennysosa2000@gmail.com>
2026-04-30 13:53:33 -0700

    4td (forgot to commit last night) day of accounting ledger capstone

--- src/main/java/com/pluralsight/Reports.java
+++ src/main/java/com/pluralsight/Reports.java
@@ -83,7 +83,7 @@ public class Reports {
                     List<Transaction> vendorResults = new ArrayList<>();
                     for (Transaction t : TransactionService.loadTransactions()) {
                         if (t.getVendor().toLowerCase().contains(vendor)) {//----- i like this piece of code here b/c it
-                            vendorResults.add(t);//---- lets the user type something like ama and itll pull up Amazon
+                            vendorResults.add(t);//---- lets the user type something like ama and itll pull up Amazon.
                         }
                     }
                     TransactionService.printTransactions(vendorResults);
